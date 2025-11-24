1ad42b853fe3c2f7e1f61b75e80c7e601698f085
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-11-23 20:49:09 -0600

    Creación de clase Conexión DB
    Contiene la lógica necesaria para conectarse a la base de datos

--- src/Db/ConexionBD.java
+++ src/Db/ConexionBD.java
@@ -1,9 +1,45 @@
 package Db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
- * @author CREADOR
+ * @author Greivin
  */
 
 public class ConexionBD {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/produccion_agricola";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Root123@";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+
+    private static ConexionBD instance;
+    private Connection connection;
+
+    private ConexionBD() {
+        try {
+            Class.forName(DRIVER);
+            this.connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ConexionBD getInstance() {
+        try {
+            if (instance == null || instance.getConnection().isClosed()) {
+                instance = new ConexionBD();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
