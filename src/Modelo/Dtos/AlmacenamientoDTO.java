6bec065dd706307830ffb67c7a23e4ac916deee6
Ricardo <richavesca@est.utn.ac.cr>
2025-11-28 16:23:34 -0600

    Se programo la clase DTOAlmacenamiento
    Se programo sin los set y sin el metodo de productos con mucho tiempo

--- src/Modelo/Dtos/AlmacenamientoDTO.java
+++ src/Modelo/Dtos/AlmacenamientoDTO.java
@@ -1,9 +1,44 @@
 package Modelo.Dtos;
 
+import java.time.LocalDate;
+
 /**
  *
- * @author CREADOR
+ * @author Ricardo Chaves
  */
 
 public class AlmacenamientoDTO {
+    private int id;
+    private String producto;
+    private double cantidad;
+    private LocalDate fechaIngreso;
+    private LocalDate fechaSalida;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public double getCantidad() {
+        return cantidad;
+    }
+
+    public LocalDate getFechaIngreso() {
+        return fechaIngreso;
+    }
+
+    public LocalDate getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public AlmacenamientoDTO(int id, String producto, double cantidad, LocalDate fechaIngreso, LocalDate fechaSalida) {
+        this.id = id;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.fechaIngreso = fechaIngreso;
+        this.fechaSalida = fechaSalida;
+    }
 }
