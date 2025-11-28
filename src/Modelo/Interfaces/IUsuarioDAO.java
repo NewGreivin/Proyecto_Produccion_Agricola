3ed24903091dd1778ce78bfb552f6f0d36e3ec8c
Ricardo <richavesca@est.utn.ac.cr>
2025-11-28 15:18:14 -0600

    Se creo programo la clase IUsuarioDAO
    Se programo con los metodos de buscarPorUsername(), validarCredenciales(), existeUsername() y listarPorRol()

--- src/Modelo/Interfaces/IUsuarioDAO.java
+++ src/Modelo/Interfaces/IUsuarioDAO.java
@@ -1,9 +1,16 @@
 package Modelo.Interfaces;
 
+import Modelo.Objetos.Usuarios.Usuario;
+import java.util.List;
+
 /**
  *
- * @author CREADOR
+ * @author Ricardo Chaves 
  */
 
 public interface IUsuarioDAO {
+    Usuario buscarPorUsername(String username);
+    boolean validarCredenciales(String username, String passwordHash);
+    boolean existeUsername(String username);
+    List<Usuario> listarPorRol(String rol);
 }
