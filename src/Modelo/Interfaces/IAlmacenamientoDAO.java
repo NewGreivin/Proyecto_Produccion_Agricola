357190bbfe80aa03edc5313c9d0cab19c4d968b9
Ricardo <richavesca@est.utn.ac.cr>
2025-11-28 16:38:43 -0600

    Se programo la clase IAlmacenamientoDAO
    Se programo la clase con los metodos de buscarPorProducto(), buscarPorFechaIngreso(), productosVencidos(), productosSinSalida()

--- src/Modelo/Interfaces/IAlmacenamientoDAO.java
+++ src/Modelo/Interfaces/IAlmacenamientoDAO.java
@@ -1,9 +1,17 @@
 package Modelo.Interfaces;
 
+import Modelo.Objetos.Almacenamientos.Almacenamiento;
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  *
- * @author CREADOR
+ * @author Ricardo Chaves
  */
 
-public interface IAlmacenamientoDAO {
+public interface IAlmacenamientoDAO extends ICRUD<Almacenamiento> {
+    List<Almacenamiento> buscarPorProducto(String nombre);
+    List<Almacenamiento> buscarPorFechaIngreso(LocalDate fecha);
+    List<Almacenamiento> productoVencidos();
+    List<Almacenamiento> productosSinSalida();
 }
