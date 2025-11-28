715b182f6023dae4824e7d464ea426eb60acfe8d
Ricardo <richavesca@est.utn.ac.cr>
2025-11-28 16:30:25 -0600

    Se programo la clase MapperAlmacenamiento
    Se programo los dos metodos de toDTO y toEntity

--- src/Modelo/Mappers/AlmacenamientoMapper.java
+++ src/Modelo/Mappers/AlmacenamientoMapper.java
@@ -1,9 +1,21 @@
 package Modelo.Mappers;
 
+import Modelo.Dtos.AlmacenamientoDTO;
+import Modelo.Objetos.Almacenamientos.Almacenamiento;
+
 /**
  *
- * @author CREADOR
+ * @author Ricardo Chaves
  */
 
 public class AlmacenamientoMapper {
+    public AlmacenamientoDTO toDTO(Almacenamiento almacenamiento){
+        AlmacenamientoDTO almacenDTO = new AlmacenamientoDTO(almacenamiento.getId(), almacenamiento.getProducto(), almacenamiento.getCantidad(), almacenamiento.getFechaIngreso(), almacenamiento.getFechaSalida());
+        return almacenDTO;
+    }
+    
+    public Almacenamiento toEntity(AlmacenamientoDTO almacenamientodto){
+        Almacenamiento almacenamiento = new Almacenamiento(almacenamientodto.getId(), almacenamientodto.getProducto(), almacenamientodto.getCantidad(), almacenamientodto.getFechaIngreso(), almacenamientodto.getFechaSalida());
+        return almacenamiento;
+    }
 }
