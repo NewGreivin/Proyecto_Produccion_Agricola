deb9dca6f1801ccedd0f39b468c16889de6a2ca5
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-11-28 23:54:19 -0600

    Se implementa la clase CultivoMapper
    Hay que corregir un error

--- src/Modelo/Mappers/CultivoMapper.java
+++ src/Modelo/Mappers/CultivoMapper.java
@@ -1,9 +1,22 @@
 package Modelo.Mappers;
 
+import Modelo.Dtos.CultivoDTO;
+import Modelo.Objetos.Cultivos.Cultivo;
+
 /**
  *
- * @author CREADOR
+ * @author MARISOL
  */
 
 public class CultivoMapper {
+    public CultivoDTO toDTO(Cultivo mp){
+        CultivoDTO cult = new CultivoDTO(mp.getId(),mp.getNombre(),mp.getTipo(),mp.getAreaSembrada(),mp.getEstado(),mp.getFechaSiembra(),mp.getFechaEstimCosecha());
+        return cult;
+    }
+
+    public Cultivo toEntity(CultivoDTO mpdto) {
+        Cultivo cult = new Cultivo(mpdto.getId(),mpdto.getNombre(),mpdto.getTipo(),mpdto.getAreaSembrada(),mpdto.getEstado(),mpdto.getFechaSiembra(),mpdto.getFechaEstimCosecha());
+
+        return cult;
+    }
 }
