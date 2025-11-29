144595a89aa85fa931e5ce499ffe90112d5755ca
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-11-28 20:15:30 -0600

    Se implementa el enum TipoCultivo

--- src/Modelo/Objetos/Cultivos/TipoCultivo.java
+++ src/Modelo/Objetos/Cultivos/TipoCultivo.java
@@ -2,8 +2,30 @@ package Modelo.Objetos.Cultivos;
 
 /**
  *
- * @author CREADOR
+ * @author MARISOL
  */
 
 public enum TipoCultivo {
+    VEGETAL("Vegetal"),
+    FRUTAL("Frutal"),
+    CEREAL("Cereal"),
+    LEGUMINOSA("Leguminosa"),
+    TUBERCULO("Tuberculo"),
+    FLOR("Flor"),
+    OTRO("Otro");
+
+    private final String descripcion;
+
+    TipoCultivo(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return descripcion;
+    }
 }
