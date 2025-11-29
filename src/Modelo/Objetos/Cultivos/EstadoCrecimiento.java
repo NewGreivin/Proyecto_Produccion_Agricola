097a595250c5b25ec978473aedf86dde64d09f15
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-11-28 20:13:36 -0600

    Se implementa el enum EstadoCrecimiento

--- src/Modelo/Objetos/Cultivos/EstadoCrecimiento.java
+++ src/Modelo/Objetos/Cultivos/EstadoCrecimiento.java
@@ -2,8 +2,28 @@ package Modelo.Objetos.Cultivos;
 
 /**
  *
- * @author CREADOR
+ * @author MARISOL
  */
 
 public enum EstadoCrecimiento {
+    SEMBRADO("Sembrado"),
+    GERMINACION("Germinacion"),
+    CRECIMIENTO("Crecimiento"),
+    MADURO("Maduro"),
+    COSECHADO("Cosechado");
+
+    private final String descripcion;
+
+    EstadoCrecimiento(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    @Override
+    public String toString() {
+        return descripcion;
+    }
 }
