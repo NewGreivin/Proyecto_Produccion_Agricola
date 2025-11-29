18a22831ffec0e10aa04d9bed1d9b6d1b42d04c9
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-11-29 16:04:13 -0600

    Actutizacion DtoCosecha
    Anteriormente en el commit (9ca590ac473f1867f8cf9cb37aa510dc86746553) no se subió correctamente la clase

--- src/Modelo/Dtos/CosechaDTO.java
+++ src/Modelo/Dtos/CosechaDTO.java
@@ -1,9 +1,36 @@
 package Modelo.Dtos;
 
+import Modelo.Objetos.Cosechas.CalidadCosecha;
+import Modelo.Objetos.Cosechas.DestinoCosecha;
+import Modelo.Objetos.Cultivos.Cultivo;
+import java.time.LocalDate;
+
 /**
  *
  * @author Greivin
  */
 
 public class CosechaDTO {
+    private final int id; //Se genera de forma Autocremental en DB
+    private final LocalDate fecha;
+    private final double cantidadRecolectada;
+    private final CalidadCosecha calidad;
+    private final DestinoCosecha destino;
+    private final Cultivo idCultivo;
+
+    public int getId() { return id; }
+    public LocalDate getFecha() { return fecha; }
+    public double getCantidadRecolectada() { return cantidadRecolectada; }
+    public CalidadCosecha getCalidad() { return calidad; }
+    public DestinoCosecha getDestino() { return destino; }
+    public Cultivo getIdCultivo() { return idCultivo; }
+
+    public CosechaDTO(int id, LocalDate fecha, double cantidadRecolectada, CalidadCosecha calidad, DestinoCosecha destino, Cultivo idCultivo) {
+        this.id = id;
+        this.fecha = fecha;
+        this.cantidadRecolectada = cantidadRecolectada;
+        this.calidad = calidad;
+        this.destino = destino;
+        this.idCultivo = idCultivo;
+    }
 }
