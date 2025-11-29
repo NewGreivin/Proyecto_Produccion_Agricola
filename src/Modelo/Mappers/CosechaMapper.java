363a2cd43bdde14db3b901475b5719a81bdfc195
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-11-29 16:12:29 -0600

    Actualizacion DtoCosecha

--- src/Modelo/Mappers/CosechaMapper.java
+++ src/Modelo/Mappers/CosechaMapper.java
@@ -1,9 +1,21 @@
 package Modelo.Mappers;
 
+import Modelo.Dtos.CosechaDTO;
+import Modelo.Objetos.Cosechas.Cosecha;
+
 /**
  *
  * @author Greivin
  */
 
 public class CosechaMapper {
+    public CosechaDTO toDTO(Cosecha cosecha){
+        CosechaDTO coseDTO = new CosechaDTO();
+        return coseDTO;
+    }
+    
+    public Cosecha toEntity(CosechaDTO dto){
+        Cosecha cose = new Cosecha();
+        return cose;
+    }
 }
