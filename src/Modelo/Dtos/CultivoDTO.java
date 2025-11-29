0eb85f46e1caf87df037031d411504194c29ed86
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-11-29 00:06:43 -0600

    Se implementa la clase CultivoDTO

--- src/Modelo/Dtos/CultivoDTO.java
+++ src/Modelo/Dtos/CultivoDTO.java
@@ -10,14 +10,42 @@ import java.time.LocalDate;
  */
 
 public class CultivoDTO {
-    private String id;
-    private String nombre;
+    private final String id;
+    private final String nombre;
     private TipoCultivo tipo;
-    private double areaSembrada;
+    private final double areaSembrada;
     private EstadoCrecimiento estado;
     private LocalDate fechaSiembra;
     private LocalDate fechaEstimCosecha;
 
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public TipoCultivo getTipo() {
+        return tipo;
+    }
+
+    public double getAreaSembrada() {
+        return areaSembrada;
+    }
+
+    public EstadoCrecimiento getEstado() {
+        return estado;
+    }
+
+    public LocalDate getFechaSiembra() {
+        return fechaSiembra;
+    }
+
+    public LocalDate getFechaEstimCosecha() {
+        return fechaEstimCosecha;
+    }
+    
     public CultivoDTO(String id,String nombre,TipoCultivo tipo,double areaSembrada, EstadoCrecimiento estado,LocalDate fechaEstimCosecha) {
         this.id = id;
         this.nombre = nombre;
@@ -27,61 +55,4 @@ public class CultivoDTO {
         this.fechaSiembra = fechaSiembra;
         this.fechaEstimCosecha = fechaEstimCosecha;
     }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public TipoCultivo getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(TipoCultivo tipo) {
-        this.tipo = tipo;
-    }
-
-    public double getAreaSembrada() {
-        return areaSembrada;
-    }
-
-    public void setAreaSembrada(double areaSembrada) {
-        this.areaSembrada = areaSembrada;
-    }
-
-    public EstadoCrecimiento getEstado() {
-        return estado;
-    }
-
-    public void setEstado(EstadoCrecimiento estado) {
-        this.estado = estado;
-    }
-
-    public LocalDate getFechaSiembra() {
-        return fechaSiembra;
-    }
-
-    public void setFechaSiembra(LocalDate fechaSiembra) {
-        this.fechaSiembra = fechaSiembra;
-    }
-
-    public LocalDate getFechaEstimCosecha() {
-        return fechaEstimCosecha;
-    }
-
-    public void setFechaEstimCosecha(LocalDate fechaEstimCosecha) {
-        this.fechaEstimCosecha = fechaEstimCosecha;
-    }
-
 }
