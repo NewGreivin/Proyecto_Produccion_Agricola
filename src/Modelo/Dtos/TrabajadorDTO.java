fda6249f934a1ad5cd76593ec93fe75a9983db66
marisolalfaro <marialfarolo@est.unt.ac.cr>
2025-11-28 22:48:22 -0600

    Se implementa la clase TrabajadorDTO

--- src/Modelo/Dtos/TrabajadorDTO.java
+++ src/Modelo/Dtos/TrabajadorDTO.java
@@ -2,8 +2,91 @@ package Modelo.Dtos;
 
 /**
  *
- * @author CREADOR
+ * @author MARISOL
  */
 
 public class TrabajadorDTO {
+    private String id;
+    private String cedula;
+    private String nombre;
+    private String telefono;
+    private String correo;
+    private String puesto;
+    private String horario;
+    private double salario;
+
+    public TrabajadorDTO(String id,String cedula,String nombre,String telefono,String correo,String puesto,String horario,double salario) {
+        this.id = id;
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+        this.puesto = puesto;
+        this.horario = horario;
+        this.salario = salario;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
 }
