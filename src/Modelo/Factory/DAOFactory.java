895b2e8b26d80e829b562f52e8c5d6221bc90067
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-11-29 18:36:26 -0600

    Creación final de DaoFactory
    Contiene todos los métodos del DaoFactory reducidos de tamaño

--- src/Modelo/Factory/DAOFactory.java
+++ src/Modelo/Factory/DAOFactory.java
@@ -2,10 +2,12 @@ package Modelo.Factory;
 
 import Modelo.Daos.AlmacenamientoMysqlDAO;
 import Modelo.Daos.CultivoMysqlDAO;
+import Modelo.Daos.ProduccionMysqlDAO;
 import Modelo.Daos.TrabajadorMysqlDAO;
 import Modelo.Daos.UsuarioMysqlDAO;
 import Modelo.Interfaces.IAlmacenamientoDAO;
 import Modelo.Interfaces.ICultivoDAO;
+import Modelo.Interfaces.IProduccionDAO;
 import Modelo.Interfaces.ITrabajadorDAO;
 import Modelo.Interfaces.IUsuarioDAO;
 
@@ -15,19 +17,9 @@ import Modelo.Interfaces.IUsuarioDAO;
  */
 
 public class DAOFactory {
-    public static IUsuarioDAO getUsuarioDAO(){
-        return new UsuarioMysqlDAO();
-    }
-    
-    public static IAlmacenamientoDAO getAlmacenamientoDAO(){
-        return new AlmacenamientoMysqlDAO();
-    }
-    
-    public static ICultivoDAO getCultivoDAO(){
-        return new CultivoMysqlDAO();
-    }
-    
-    public static ITrabajadorDAO getTrabajadorDAO(){
-       return new TrabajadorMysqlDAO(); 
-    }
+    public static IUsuarioDAO crearUsuarioDAO(){ return new UsuarioMysqlDAO(); }
+    public static IAlmacenamientoDAO crearAlmacenamientoDAO(){ return new AlmacenamientoMysqlDAO(); }
+    public static IProduccionDAO crearProduccionDAO(){ return new ProduccionMysqlDAO(); }  
+    public static ICultivoDAO crearCultivoDAO(){ return new CultivoMysqlDAO(); }
+    public static ITrabajadorDAO crearTrabajadorDAO(){ return new TrabajadorMysqlDAO();  }
 }
