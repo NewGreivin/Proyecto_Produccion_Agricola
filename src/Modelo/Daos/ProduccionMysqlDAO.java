193ae54b5d04770904c5d1c2b031bc1c2770a8f8
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-11-30 22:26:33 -0600

    Actualizacion para hacer BUILD

--- src/Modelo/Daos/ProduccionMysqlDAO.java
+++ src/Modelo/Daos/ProduccionMysqlDAO.java
@@ -1,9 +1,16 @@
 package Modelo.Daos;
 
+import Db.ConexionBD;
+import Modelo.Dtos.ProduccionDTO;
 import Modelo.Interfaces.IProduccionDAO;
-import Modelo.Objetos.Produccion.Produccion;
+import Modelo.Mappers.ProduccionMapper;
+import java.sql.Connection;
+import java.sql.PreparedStatement; 
 import java.time.LocalDate;
 import java.util.List;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.ResultSet;
 
 /**
  *
@@ -11,50 +18,72 @@ import java.util.List;
  */
 
 public class ProduccionMysqlDAO implements IProduccionDAO {
+    private final ConexionBD conexion;
+        private final ProduccionMapper mapper;
 
+    public ProduccionMysqlDAO() {
+        conexion = ConexionBD.getInstance();
+        mapper = new ProduccionMapper();
+    }
+    
     @Override
-    public List<Produccion> buscarPorRangoFecha(LocalDate fechaInicio, LocalDate fechaFin) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<ProduccionDTO> buscarPorRangoFecha(LocalDate fechaInicio, LocalDate fechaFin) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public List<Produccion> buscarPorCultivo(String idCultivo) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<ProduccionDTO> buscarPorCultivo(String idCultivo) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public List<Produccion> buscarPorDestino(String destino) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<ProduccionDTO> buscarPorDestino(String destino) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public List<Produccion> buscarPorFecha(String fecha) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<ProduccionDTO> buscarPorFecha(String fecha) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public List<Produccion> buscarPorCalidad(String calidad) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<ProduccionDTO> buscarPorCalidad(String calidad) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public void crear(Produccion t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void crear(ProduccionDTO t) {
+        String sql = "INSERT INTO produccion (fecha, cantidad_recolectada, calidad, destino, id_cultivo) VALUES (?, ?, ?, ?, ?)";
+        
+        try (Connection conn = conexion.getConnection();
+             PreparedStatement pst = conn.prepareStatement(sql)) {
+            
+            pst.setString(1, t.getFecha().toString());
+            pst.setDouble(2, t.getCantidadRecolectada());
+            pst.setString(3, t.getCalidad().name());
+            pst.setString(4, t.getDestino().name());
+            pst.setString(5, t.getIdCultivo().getId());
+            pst.executeUpdate();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
-    public List<Produccion> listar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<ProduccionDTO> listar() {
+    throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public void actualizar(Produccion t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public void actualizar(ProduccionDTO t) {
+
     }
 
     @Override
     public void eliminar(String t) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+
     }
-    
+
 }
