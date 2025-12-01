b4de92efb397dcda9769f06da95e4dd26b3d9abe
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-11-30 21:59:08 -0600

    Actualización interfaz Dao
    Contiene el cambio de objetos Produccion a ProduccionDTO

--- src/Modelo/Interfaces/IProduccionDAO.java
+++ src/Modelo/Interfaces/IProduccionDAO.java
@@ -1,6 +1,6 @@
 package Modelo.Interfaces;
 
-import Modelo.Objetos.Produccion.Produccion;
+import Modelo.Dtos.ProduccionDTO;
 import java.time.LocalDate;
 import java.util.List;
 
@@ -8,10 +8,10 @@ import java.util.List;
  * @author Greivin
  */
 
-public interface IProduccionDAO extends ICRUD<Produccion> {
-    List<Produccion> buscarPorRangoFecha(LocalDate fechaInicio, LocalDate fechaFin);
-    List<Produccion> buscarPorCultivo(String idCultivo);
-    List<Produccion> buscarPorDestino(String destino);
-    List<Produccion> buscarPorFecha(String fecha);
-    List<Produccion> buscarPorCalidad(String calidad);
+public interface IProduccionDAO extends ICRUD<ProduccionDTO> {
+    List<ProduccionDTO> buscarPorRangoFecha(LocalDate fechaInicio, LocalDate fechaFin);
+    List<ProduccionDTO> buscarPorCultivo(String idCultivo);
+    List<ProduccionDTO> buscarPorDestino(String destino);
+    List<ProduccionDTO> buscarPorFecha(String fecha);
+    List<ProduccionDTO> buscarPorCalidad(String calidad);
 }
