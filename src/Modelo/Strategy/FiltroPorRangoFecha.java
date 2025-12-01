bde094dfc5546d1153ffe502196f57ba3d0ba2e8
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-12-01 17:47:06 -0600

    Se creo FiltroPorRangoFecha
    Contiene su  método de la interfaz configurado así como su método y constructor necesario

--- src/Modelo/Strategy/FiltroPorRangoFecha.java
+++ src/Modelo/Strategy/FiltroPorRangoFecha.java
@@ -1,9 +1,39 @@
 package Modelo.Strategy;
 
+import Modelo.Objetos.Cultivos.Cultivo;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author CREADOR
+ * @author Greivin
  */
 
-public class FiltroPorRangoFecha {
+public class FiltroPorRangoFecha implements IFiltroCultivoStrategy {
+    
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+
+    public FiltroPorRangoFecha(LocalDate fechaInicio, LocalDate fechaFin) {
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+    }
+    
+    @Override
+    public List<Cultivo> filtrar(List<Cultivo> cultivos) {
+        List<Cultivo> resultado = new ArrayList<>();
+        
+        for (int i = 0; i < cultivos.size(); i++) {
+            Cultivo cultivo = cultivos.get(i);
+            LocalDate fechaSiembra = cultivo.getFechaSiembra();
+            
+            if (fechaSiembra != null && 
+                !fechaSiembra.isBefore(fechaInicio) && 
+                !fechaSiembra.isAfter(fechaFin)) {
+                resultado.add(cultivo);
+            }
+        }
+        
+        return resultado;
+    }
 }
