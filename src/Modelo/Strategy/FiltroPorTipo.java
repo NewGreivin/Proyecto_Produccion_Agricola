57a55b232fb4e824ae911325827a040ce8925dda
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-12-01 17:47:23 -0600

    Creacion de FiltroPorTipo
    Contiene su  método de la interfaz configurado así como su método y constructor necesario

--- src/Modelo/Strategy/FiltroPorTipo.java
+++ src/Modelo/Strategy/FiltroPorTipo.java
@@ -1,9 +1,30 @@
 package Modelo.Strategy;
 
+import Modelo.Objetos.Cultivos.Cultivo;
+import Modelo.Objetos.Cultivos.TipoCultivo;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- *
- * @author CREADOR
+ * @author Greivin
  */
 
-public class FiltroPorTipo {
+public class FiltroPorTipo implements IFiltroCultivoStrategy {
+    private TipoCultivo tipo;
+    
+    public FiltroPorTipo(TipoCultivo tipo) { this.tipo = tipo; }
+
+    @Override
+    public List<Cultivo> filtrar(List<Cultivo> cultivos) {
+        List<Cultivo> resultado = new ArrayList<>();
+        
+        for (int i = 0; i < cultivos.size(); i++) {
+            Cultivo cultivo = cultivos.get(i);
+            if (cultivo.getTipo() == tipo) {
+                resultado.add(cultivo);
+            }
+        }
+        
+        return resultado;
+    }
 }
