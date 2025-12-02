5a504c8e028c3b32fb537646f8c2487141ddba51
Ricardo <richavesca@est.utn.ac.cr>
2025-12-01 22:19:02 -0600

    Se descomento el servicioUsuario
    Gerivin andava gracioso y me dejo comentado mi servicio

--- src/Modelo/Facade/ServicioFacade.java
+++ src/Modelo/Facade/ServicioFacade.java
@@ -4,6 +4,7 @@ import Modelo.Servicios.ServicioAlmacenamiento;
 import Modelo.Servicios.ServicioCultivo;
 import Modelo.Servicios.ServicioProduccion;
 import Modelo.Servicios.ServicioTrabajador;
+import Modelo.Servicios.ServicioUsuario;
 
 /**
  * @author Greivin
@@ -16,20 +17,20 @@ public class ServicioFacade {
     private final ServicioCultivo servicioCultivo;
     private final ServicioProduccion servicioProduccion;
     private final ServicioTrabajador servicioTrabajador;
-    //private final ServicioUsuario servicioUsuario;
+    private final ServicioUsuario servicioUsuario;
 
     public ServicioAlmacenamiento getServicioAlmacenamiento() { return servicioAlmacenamiento; }
     public ServicioCultivo getServicioCultivo() { return servicioCultivo; }
     public ServicioProduccion getServicioProduccion() { return servicioProduccion; }
     public ServicioTrabajador getServicioTrabajador() { return servicioTrabajador; }
-    //public ServicioUsuario getServicioUsuario() { return servicioUsuario; }
+    public ServicioUsuario getServicioUsuario() { return servicioUsuario; }
 
     public ServicioFacade() {
         this.servicioAlmacenamiento = new ServicioAlmacenamiento();
         this.servicioCultivo = new ServicioCultivo();
         this.servicioProduccion = new ServicioProduccion();
         this.servicioTrabajador = new ServicioTrabajador();
-        //this.servicioUsuario = new ServicioUsuario();
+        this.servicioUsuario = new ServicioUsuario();
     }
 
     public static ServicioFacade getInstancia() {
