f418fe4fe37f30e9c877aa46685ea52fd974c82f
Ricardo <richavesca@est.utn.ac.cr>
2025-12-02 15:18:32 -0600

    Se hisieron de nuevo las tablas
    Se crearon nuevamente las tablas con los cambios propuestos anteriormente por el grupo, sobre el AUTO_INCREMENT

---
+++ initdb/001 - CreacionDeTablas.sql
@@ -0,0 +1,211 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.3
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: mariadb:3306
+-- Tiempo de generación: 02-12-2025 a las 21:14:41
+-- Versión del servidor: 11.8.3-MariaDB-ubu2404
+-- Versión de PHP: 8.3.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ProduccionAgricola`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `almacenamiento`
+--
+
+CREATE TABLE `almacenamiento` (
+  `id` int(11) NOT NULL,
+  `producto` varchar(20) NOT NULL,
+  `cantidad` double NOT NULL,
+  `fecha_ingreso` date NOT NULL,
+  `fecha_salida` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cultivos`
+--
+
+CREATE TABLE `cultivos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `tipo` varchar(15) NOT NULL,
+  `area_sembrada` double NOT NULL,
+  `estado_crecimiento` varchar(15) NOT NULL,
+  `fecha_siembra` date NOT NULL,
+  `fecha_estim_cosecha` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Volcado de datos para la tabla `cultivos`
+--
+
+INSERT INTO `cultivos` (`id`, `nombre`, `tipo`, `area_sembrada`, `estado_crecimiento`, `fecha_siembra`, `fecha_estim_cosecha`) VALUES
+(1, 'Maíz', 'Grano', 10.5, 'Activo', '2025-03-01', '2025-06-01');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `produccion`
+--
+
+CREATE TABLE `produccion` (
+  `id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `cantidad_recolectada` double NOT NULL,
+  `calidad` varchar(15) NOT NULL,
+  `destino` varchar(18) NOT NULL,
+  `id_cultivo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Volcado de datos para la tabla `produccion`
+--
+
+INSERT INTO `produccion` (`id`, `fecha`, `cantidad_recolectada`, `calidad`, `destino`, `id_cultivo`) VALUES
+(1, '2025-12-02', 250, 'Alta', 'Bodega', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `trabajadores`
+--
+
+CREATE TABLE `trabajadores` (
+  `cedula` varchar(15) NOT NULL,
+  `nombre` varchar(40) NOT NULL,
+  `telefono` varchar(15) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `puesto` varchar(30) NOT NULL,
+  `horario` varchar(20) NOT NULL,
+  `salario` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Volcado de datos para la tabla `trabajadores`
+--
+
+INSERT INTO `trabajadores` (`cedula`, `nombre`, `telefono`, `correo`, `puesto`, `horario`, `salario`) VALUES
+('118450236', 'Juan Perez', '88881234', 'juan@gmail.com', 'Supervisor', 'Diurno', 550000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `rol` varchar(18) NOT NULL,
+  `id_trabajador` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `username`, `password_hash`, `rol`, `id_trabajador`) VALUES
+(1, 'ricardo', 'hash123', 'ADMIN', '118450236');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `almacenamiento`
+--
+ALTER TABLE `almacenamiento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cultivos`
+--
+ALTER TABLE `cultivos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `produccion`
+--
+ALTER TABLE `produccion`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_produccion_cultivo` (`id_cultivo`);
+
+--
+-- Indices de la tabla `trabajadores`
+--
+ALTER TABLE `trabajadores`
+  ADD PRIMARY KEY (`cedula`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `fk_usuario_trabajador` (`id_trabajador`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `almacenamiento`
+--
+ALTER TABLE `almacenamiento`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cultivos`
+--
+ALTER TABLE `cultivos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `produccion`
+--
+ALTER TABLE `produccion`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `produccion`
+--
+ALTER TABLE `produccion`
+  ADD CONSTRAINT `fk_produccion_cultivo` FOREIGN KEY (`id_cultivo`) REFERENCES `cultivos` (`id`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `fk_usuario_trabajador` FOREIGN KEY (`id_trabajador`) REFERENCES `trabajadores` (`cedula`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
