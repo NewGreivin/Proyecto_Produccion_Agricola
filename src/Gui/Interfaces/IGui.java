e10befc4547a67a198f280d1f41e48a0d10b7f7c
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-12-07 13:40:04 -0600

    Creacion de iGui
    Contiene la logica de metodos general de las interfaces graficas,

---
+++ src/Gui/Interfaces/IGui.java
@@ -0,0 +1,14 @@
+/**
+ * @author Greivin
+ */
+package Gui.Interfaces;
+
+public interface IGui {
+    public void save();
+    public void clear();
+    public void delete();
+    public void update();
+    public void search();
+    public boolean validateRequiere();
+    public void showdata();
+}
