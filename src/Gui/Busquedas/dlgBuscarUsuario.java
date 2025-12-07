a08a8a0ae34d00ef801e474b2da8123f76fbfd13
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-12-07 16:09:39 -0600

    Actualización DlgBuscarUsuario
    Se agrego el campo de trabajador que hacia falta

--- src/Gui/Busquedas/dlgBuscarUsuario.java
+++ src/Gui/Busquedas/dlgBuscarUsuario.java
@@ -78,14 +78,14 @@ public class dlgBuscarUsuario extends javax.swing.JDialog {
 
             },
             new String [] {
-                "Id", "Usuario", "Contraseña", "Rol"
+                "Id", "Usuario", "Contraseña", "Rol", "Trabajador"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
             boolean[] canEdit = new boolean [] {
-                false, false, false, false
+                false, false, false, false, false
             };
 
             public Class getColumnClass(int columnIndex) {
